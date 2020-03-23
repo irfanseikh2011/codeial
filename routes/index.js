3e7b418d9396db7c895e9c5de1f7eb015b7e568b
Irfan Sheikh <seikhirfan2011@gmail.com>
2020-03-23 11:57:43 +0530

    added a route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
-module.exports = router;
 
-console.log("router loaded");
+const router = express.Router;
+
+const homeController = require('./controllers/home_controller');
+
+
+router.get('/',homeController.home);
+
+module.exports = router;
