a0e178f74dcd8be039ddfa77ef8fb60a596eea62
Irfan Sheikh <seikhirfan2011@gmail.com>
2020-03-23 15:37:12 +0530

    Mongoose Setup

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db = mongoose.connection;
+
+
+db.on('error',console.error.bind(console,"Error in DB"));
+
+
+db.once('open',function(){
+    console.log("Connected to DB");
+});
+
+
+module.exports = db;
