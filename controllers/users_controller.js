93fe8ae84d6d9542c38ae40c5aeb5dc33001cef5
Irfan Sheikh <seikhirfan2011@gmail.com>
2020-04-05 21:51:14 +0530

    Debugged

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,7 +4,6 @@ const User = require('../models/user');
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
-        user:
     })
 }
 
