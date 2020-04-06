4d257c97441fdc66c092816c04a32a431590dd77
Irfan Sheikh <seikhirfan2011@gmail.com>
2020-04-06 22:54:07 +0530

    changed the UI

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,11 @@
 module.exports.home = function(req, res){
+
+    if(!req.isAuthenticated())
+    {
+      return  res.redirect('/users/sign-in');
+    }
+
+
     console.log(req.cookies);
     res.cookie('user_id', 25);
     return res.render('home', {
