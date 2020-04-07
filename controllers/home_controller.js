8c89dde46eb817b1f051a04ffdfd9e4a8da13572
Irfan Sheikh <seikhirfan2011@gmail.com>
2020-04-07 19:49:45 +0530

    changed the UI for the home page and showing the person who posted

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,4 @@
-const Post = require('../models/posts');
+const Post = require('../models/post');
 
 
 module.exports.home = function(req, res){
@@ -7,14 +7,13 @@ module.exports.home = function(req, res){
     {
       return  res.redirect('/users/sign-in');
     }
-
-
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-    return res.render('home', {
+    Post.find({}).populate('user').exec(function(err,posts){
+      return res.render('home', {
         title: "Codeial | Home",
         posts : posts
     });
+  });
+    
 }
 
 // module.exports.actionName = function(req, res){}
